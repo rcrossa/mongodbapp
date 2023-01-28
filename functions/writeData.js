ccb9fce5792cae1c216ed5f65a98fc3c0025d2c3
Roberto Rossa <rcrossa@hotmail.com>
2023-01-28 20:33:21 -0300

    first commit

---
+++ functions/writeData.js
@@ -0,0 +1,13 @@
+// This function is the endpoint's request handler.
+exports = function({ query, headers, body}, response) {
+    
+    // Querying a mongodb service:
+     const doc = context.services.get("mongodb-atlas").db("mongodbworld").collection("2022").insertOne(JSON.parse(body.tex()));
+
+    // Calling a function:
+    // const result = context.functions.execute("function_name", arg1, arg2);
+
+    // The return value of the function is sent as the response back to the client
+    // when the "Respond with Result" setting is set.
+    return  doc;
+};
